676d4cb4cb707f21cbf482a844f585d88cb3eb51
IDDT <IDDT@users.noreply.github.com>
2021-03-11 10:51:57 +0700

    Version bump.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='thai_tokenizer',
-    version='0.2.1',
+    version='0.2.2',
     description='Fast and accurate Thai tokenization library.',
     url='https://github.com/IDDT/thai-tokenizer',
     author='Kirill Orlov',
